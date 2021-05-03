904ed38f1752587b2ce049fb7ecc2112edc5b9d3
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-05-03 10:18:38 +0300

    Initial commit

---
+++ src/main/java/com/example/servicea/Controller.java
@@ -0,0 +1,25 @@
+package com.example.servicea;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping(path = "/a")
+    public String response(){
+        return "Response from A";
+    }
+
+    @GetMapping(path = "/ab")
+    public String responseFromB(){
+        return template.getForEntity("http://service-b/b", String.class).getBody();
+    }
+
+}
+
